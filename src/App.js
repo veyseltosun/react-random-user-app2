8465ade88e2445eea9ef39ec2ec8dc6950a7da97
veyseltosun <postalarim1923@gmail.com>
2022-12-13 20:23:28 +0300

    hooks.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const url = "https://randomuser.me/api/"
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  const getUser = () => {
+    axios.get(url).then(function (response) {
+      console.log(response.data.results);
+      setUser(response.data.results)
+    })
+  }
+
+
+  useEffect(() => {
+    getUser()
+  }, [])
   return (
     <div className="App">
-     
+      
     </div>
   );
 }
