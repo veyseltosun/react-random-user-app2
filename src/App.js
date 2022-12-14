d91d4ddf84792af84929c5f345f1bdfc6d4a31d9
veyseltosun <postalarim1923@gmail.com>
2022-12-14 03:04:49 +0300

    strcutures geting pile up.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { useState, useEffect } from 'react';
+import Footer from "./components/Footer"
 import axios from 'axios';
+import mailSvg from "./assets/mail.svg";
+import manSvg from "./assets/man.svg";
+import womanSvg from "./assets/woman.svg";
+import manAgeSvg from "./assets/growing-up-man.svg";
+import womanAgeSvg from "./assets/growing-up-woman.svg";
+import mapSvg from "./assets/map.svg";
+import phoneSvg from "./assets/phone.svg";
+import padlockSvg from "./assets/padlock.svg";
+import cwSvg from "./assets/cw.svg";
 
 const url = "https://randomuser.me/api/"
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
 function App() {
 
   const [user, setUser] = useState(null);
@@ -29,6 +41,10 @@ function App() {
           </div>
         )
       })}
+
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
